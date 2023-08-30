21c9dcefeb5e566bc5b7ed92c59a9ffaabc7b72c
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2023-08-30 15:20:00 +0530

    Relese 2.0

---
+++ invento/ticketList/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from ticketList import views
+
+urlpatterns = [   
+    path('', views.index, name='home'),
+    path('new', views.addTkt, name='addticket'),
+    path('upload', views.upload, name='upload'),
+    path('redeem', views.redeem, name='redeem'),
+    path('signin', views.signin, name='signin'),
+    path('invento_login', views.invento_login, name='invento_login'),
+    path('invento_logout', views.invento_logout, name='invento_logout'),
+    path('search_ticket', views.search_ticket, name='search_ticket'),
+    path('delete', views.delete, name='delete'),
+]
+ 
